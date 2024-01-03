d35623b1ba3bc7f86f2ab66f8f9e58bbdfdeb43e
Per Turner <per.turner@outlook.com>
2024-01-03 15:40:37 +0100

    add Fish script

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -Ua SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 end
 
 set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 set -gx VISUAL code --wait
 set -gx EDITOR nano
+set -gx GITHUB_USERNAME devloloper
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
