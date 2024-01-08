d3120cdc67f8ad7388c7b7db975f274c4952fc5b
Per Turner <per.turner@outlook.com>
2024-01-08 20:22:37 +0100

    Fix fish, hyprland mouse

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -Ua SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 end
+
+# Add ssh key to keyring
+set -U SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 
 set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 set -gx VISUAL code --wait
@@ -10,3 +12,4 @@ set -gx GITHUB_USERNAME devloloper
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
+fish_add_path -aP /opt/rocm/bin
