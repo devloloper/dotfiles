102b192860c3bb502c2cef250697ae242d0368d0
Per Turner <per.turner@outlook.com>
2024-12-14 15:33:16 +0100

    Update Hyprland config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,6 +5,8 @@ end
 # Add ssh key to keyring
 set -U SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 
+set -gx MOZ_ENABLE_WAYLAND 1
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
 set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 set -gx VISUAL code --wait
 set -gx EDITOR nano
