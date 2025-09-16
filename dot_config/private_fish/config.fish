dc118a1b20734721377ca75273ce0938f8a56c4e
Per Turner <per.turner@outlook.com>
2025-09-16 21:48:26 +0200

    Add starship

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,13 +10,13 @@ set -U SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
-set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 set -gx VISUAL code --wait
 set -gx EDITOR nano
 set -gx GITHUB_USERNAME devloloper
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
-fish_add_path -aP /opt/rocm/bin
 
 pyenv init - | source
+
+starship init fish | source
