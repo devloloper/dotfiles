eb01e4bfdb6bf55804e18f76247879103875e8af
Per Turner <per.turner@outlook.com>
2025-11-30 09:33:42 +0100

    Start using pywal

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+   # Commands to run in interactive sessions can go here
     if uwsm check may-start
         exec uwsm start hyprland
     end
-end
-
-# Add .local/bin to PATH if it exists
-if test -d "$HOME/.local/bin"
-    fish_add_path "$HOME/.local/bin"
 end
 
 # Add ssh key to keyring
@@ -15,13 +10,13 @@ set -U SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
 
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
+set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
 set -gx VISUAL code --wait
 set -gx EDITOR nano
 set -gx GITHUB_USERNAME devloloper
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
+fish_add_path -aP /opt/rocm/bin
 
 pyenv init - | source
-
-starship init fish | source
