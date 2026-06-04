00ee322866b790c46bebdb19d04c38bde45af239
Per Turner <per@turnertech.dev>
2026-06-04 12:12:59 +0200

    telescope: add fzf-native, literal grep, and find-all-files picker

--- common/.config/nvim/lua/plugins/telescope.lua
+++ common/.config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,23 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     keys = {
         { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+        { "<leader>fA", "<cmd>Telescope find_files no_ignore=true<cr>", desc = "Find All Files (incl. ignored)" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep (regex)" },
+        { "<leader>fs", function()
+            require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+        end, desc = "Grep String (literal)" },
         { "<leader><leader>", "<cmd>Telescope buffers<cr>", desc = "Switch Buffers" },
         { "<C-e>", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
     },
+    config = function(_, opts)
+        require("telescope").setup(opts)
+        require("telescope").load_extension("fzf")
+    end,
     opts = {
         pickers = {
             find_files = {
